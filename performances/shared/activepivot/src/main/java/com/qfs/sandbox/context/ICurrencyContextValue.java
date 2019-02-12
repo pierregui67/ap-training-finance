29ffab42245e4c628247757558880f50cbe63987
Nawfel <nawfel.mestoui@globms.com>
2019-02-12 16:37:28 +0100

    CurrencyContextValue

---
+++ performances/shared/activepivot/src/main/java/com/qfs/sandbox/context/ICurrencyContextValue.java
@@ -0,0 +1,8 @@
+package com.qfs.sandbox.context;
+
+import com.quartetfs.biz.pivot.context.IContextValue;
+
+public interface ICurrencyContextValue extends IContextValue {
+
+    String getCurrency();
+}
